5ff2db067e0013ba8976ee5710d527121fd385ea
ZoOmmyQ <z0om198@yandex.ru>
2024-02-18 20:19:13 +0300

    delete cli

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-const playgame = () => {
-let userName = readlineSync.question('May I have your name? ');
-
-console.log('Hello ' + userName + '!');
-};
-export default playgame;
