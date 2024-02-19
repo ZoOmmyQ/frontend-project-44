fcb402747ec24602cbb72ed5efbca07bd9885881
ZoOmmyQ <z0om198@yandex.ru>
2024-02-19 08:19:05 +0300

    changes

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -5,10 +5,9 @@ import playGame from '../index.js';
 const rule = 'Find the greatest common divisor of given numbers. ';
 
 const getCorrectAnswer = (operand1, operand2) => {
-  if (operand2 > operand1) {
-    const gcd = operand2 % operand1;
-    return getCorrectAnswer(operand2, gcd);
-  } return Math.abs(operand1);
+  if (!operand2) {
+    return operand1;
+  } return getCorrectAnswer(operand2, operand1 % operand2);
 };
 const getQuestionAndAnswer = () => {
   const operand1 = getRandomNumber();
