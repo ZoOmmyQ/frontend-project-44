8c4d69518e6b4a3eafdc396f4897f5db95c389b1
ZoOmmyQ <z0om198@yandex.ru>
2024-03-30 16:02:21 +0300

    answer

--- src/utils/getStringAnswer.js
+++ src/utils/getStringAnswer.js
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-const getStringAnswer = (question) => question ? 'yes' : 'no';
-=======
 const getStringAnswer = (question) => (question ? 'yes' : 'no');
 
->>>>>>> Stashed changes
 export default getStringAnswer;
